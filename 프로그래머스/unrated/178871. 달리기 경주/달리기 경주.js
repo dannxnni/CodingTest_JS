63fb65c2a2ed3844864735284879839b4300b7fc
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-07-10 15:39:23 +0900

    [unrated] Title: 달리기 경주, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.js
@@ -0,0 +1,18 @@
+function solution(players, callings) {
+    const map = new Map();
+    
+    players.forEach((name, index) => {
+        map.set(name, index);
+    });
+    
+    callings.forEach(name => {
+        const curIdx = map.get(name);
+        const front = players[curIdx - 1];
+        
+        [players[curIdx], players[curIdx - 1]] = [players[curIdx - 1], players[curIdx]];
+        
+        map.set(name, map.get(name) - 1);
+        map.set(front, map.get(name) + 1);
+    });
+    return players;
+}
