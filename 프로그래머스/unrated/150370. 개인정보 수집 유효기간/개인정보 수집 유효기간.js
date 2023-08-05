3d8326439063cd100a8b4c811d9bf29106f8ced4
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-08-05 19:24:22 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.62 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -0,0 +1,33 @@
+function solution(today, terms, privacies) {
+    const todayDate = Number(today.split('.').join(''));
+    let termsObj = {};
+    
+    terms.forEach((term, index) => {
+        const [contract, month] = term.split(' ');
+        termsObj[contract] = Number(month);
+    });
+    let ans = [];
+    
+    privacies.forEach((privacy, index) => {
+        let [date, contract] = privacy.split(' ');
+        let [year, month, day] = date.split('.').map(Number);
+        
+        month += termsObj[contract];
+        
+        while(month > 12) {
+            year += 1;
+            month -= 12;
+        }
+        
+        year = String(year);
+        month = String(month).padStart(2, '0');
+        day = String(day).padStart(2, '0');
+        
+        const calDate = Number(year + month + day);
+        
+        if (calDate <= todayDate) {
+            ans.push(index + 1);
+        }
+    });
+    return ans;
+}
