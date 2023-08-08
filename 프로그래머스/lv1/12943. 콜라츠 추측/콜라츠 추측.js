79b7cea61dba013a7d606d987b37df248d73e923
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-08-08 15:32:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,15 @@
+function solution(num) {
+    if (num === 1) {
+        return 0;
+    } else {
+        let cnt = 0;
+        
+        while (num !== 1) {
+            if (cnt === 500)    return -1;
+            if (num % 2 === 0)  num /= 2;
+            else     num = num * 3 + 1;
+            cnt++;
+        }
+        return cnt;
+    }
+}
