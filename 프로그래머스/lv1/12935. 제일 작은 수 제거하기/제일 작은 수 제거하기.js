4355e81078006193b7ccf1fd89ad78b8195d581b
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-08-08 16:17:40 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,10 +1,5 @@
 function solution(arr) {
-    if (arr.length === 1)   return [-1];
-    else {
-        let min = Math.min(...arr);
-        let idx = arr.indexOf(min);
-        
-        arr.splice(idx, 1);
-        return arr;
-    }
+    arr.splice(arr.indexOf(Math.min(...arr)), 1);
+    if (arr.length < 1) return [-1];
+    return arr;
 }
