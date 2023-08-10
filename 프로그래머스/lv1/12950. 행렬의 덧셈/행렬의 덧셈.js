7cbfb417ebeb3babf4c928b0a2f3046a829e6c2f
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-08-10 16:32:39 +0900

    [level 1] Title: 행렬의 덧셈, Time: 8.88 ms, Memory: 67.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(arr1, arr2) {
+    let ans = [];
+    
+    for (let i = 0; i < arr1.length; i++) {
+        let arr = [];
+        
+        for (let j = 0; j < arr1[0].length; j++) {
+            let sum = arr1[i][j] + arr2[i][j];
+            arr.push(sum);
+        }
+        ans.push(arr);
+    }
+    return ans;
+}
