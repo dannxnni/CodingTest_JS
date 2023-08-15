06cc69e64f0697a91ecbb708c9d1cc4acf04217f
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-08-15 16:22:37 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -0,0 +1,8 @@
+function solution(brown, yellow) {
+    var ans = [];
+    for (var y = 3; y <= (brown + yellow) / y; y++) {
+        var x = Math.floor((brown + yellow) / y);
+        if ((x - 2) * (y - 2) === yellow)   break;
+    }
+    return [x, y];
+}
