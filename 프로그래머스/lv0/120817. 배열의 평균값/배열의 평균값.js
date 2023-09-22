cb97df29ddef6c822f4e7aba9f94a4af3c6d1365
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-09-22 18:16:08 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,7 +1,3 @@
 function solution(numbers) {
-    let sum = 0;
-    numbers.forEach(n => {
-        sum += n;
-    });
-    return sum / numbers.length;
+    return numbers.reduce((a, b) => a + b, 0) / numbers.length;
 }
