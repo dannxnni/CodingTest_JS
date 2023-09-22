c5b2d88462b73a7fe69b7d8e0fbc9587a30d360c
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-09-22 17:28:04 +0900

    [level 0] Title: 짝수의 합, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++ 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
@@ -1,8 +1,8 @@
 function solution(n) {
     let sum = 0;
     
-    for (let i = 1; i <= n; i++) {
-        if (i % 2 === 0)    sum += i;
+    for (let i = 2; i <= n; i+=2) {
+        sum += i;
     }
     return sum;
 }
