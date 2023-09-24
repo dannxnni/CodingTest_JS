297ec85829c92dc2d88c2d2ce7fc716f06c79a73
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-09-24 18:30:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
+++ 프로그래머스/lv0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -1,8 +1,3 @@
 function solution(array, height) {
-    let cnt = 0;
-    
-    for (let i of array) {
-        if (i > height) cnt++;
-    }
-    return cnt;
+    return array.filter((i) => i > height).length;
 }
