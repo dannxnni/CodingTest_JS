3c33fc80bb766f7eb094d73034cc5fdcbe8fd97a
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-09-25 15:01:38 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/lv0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,8 +1,3 @@
 function solution(my_string, letter) {
-    let ans = "";
-    let str = my_string.split("");
-    for (let i = 0; i < str.length; i++) {
-        if (str[i] !== letter)  ans += str[i];
-    }
-    return ans;
+    return my_string.split(letter).join("");
 }
