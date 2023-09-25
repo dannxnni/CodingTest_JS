d5f37f9b7d2fb81883a9684f7a57edb48639ae4d
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-09-25 15:23:44 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
+++ 프로그래머스/lv0/120908. 문자열안에 문자열/문자열안에 문자열.js
@@ -1,6 +1,3 @@
 function solution(str1, str2) {
-    for (let i = 0; i < str1.length; i++) {
-        if (str1.includes(str2))    return 1;
-        else    return 2;
-    }
+    return str1.includes(str2) ? 1 : 2;
 }
