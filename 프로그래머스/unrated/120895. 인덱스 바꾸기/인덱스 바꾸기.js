8a5f580304b4bb4cf5d143107ec9143500a9431f
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-11-24 09:38:01 +0900

    [level unrated] Title: 인덱스 바꾸기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+    let str = [...my_string];
+    str.splice(num1, 1, my_string[num2]);
+    str.splice(num2, 1, my_string[num1]);
+    
+    return str.join("");
+}
