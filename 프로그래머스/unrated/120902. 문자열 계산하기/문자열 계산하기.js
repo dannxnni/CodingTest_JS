68cdcb0fe62e282089a683e31cec1e609c0ea38f
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-12-18 22:56:57 +0900

    [level unrated] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let arr = my_string.split(" ");
+    let ans = Number(arr[0]);
+    
+    for (let i = 1; i < arr.length; i += 2) {
+        arr[i] === "+" ? ans += Number(arr[i + 1]) : ans -= Number(arr[i + 1])
+    }
+    return ans;
+}
