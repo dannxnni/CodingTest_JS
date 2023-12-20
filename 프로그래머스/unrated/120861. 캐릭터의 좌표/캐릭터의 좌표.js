20f94fb61b9e91836323ca517e17db902bc6942c
dannxnni <132893321+dannxnni@users.noreply.github.com>
2023-12-20 23:11:09 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,18 +1,13 @@
 function solution(keyinput, board) {
-    const cur = [0, 0]
-    const key = {
-        up : [0, 1],
-        down : [0, -1], 
-        left : [-1, 0],
-        right : [1, 0]
-    }
-    const max = [Math.floor(board[0] / 2), Math.floor(board[1] / 2)]
+    let res = [0, 0];
     
-    keyinput.forEach(i => {
-        if (Math.abs(cur[0] + key[i][0]) <= max[0] && Math.abs(cur[1] + key[i][1]) <= max[1]) {
-            cur[0] += key[i][0]
-            cur[1] += key[i][1]
+    for (let i of keyinput) {
+        switch(i) {
+            case "up": if (res[1] < board[1] / 2 - 1)  res[1]++;    break;
+            case "down": if (-res[1] < board[1] / 2 - 1) res[1]--;  break;
+            case "left": if (-res[0] < board[0] / 2 - 1)    res[0]--; break;
+            case "right": if (res[0] < board[0] / 2 - 1)    res[0]++; break;
         }
-    })
-    return cur;
+    }
+    return res;
 }
